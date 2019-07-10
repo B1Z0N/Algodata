06f550855bf86d7eaa8b3bd7703e52289cdccd8e
B1Z0N <kolausf@gmail.com>
2019-07-10 19:11:17 +0300

    Written basic avl-specific code, not tested yet

--- AVL/AVL.hpp
+++ AVL/AVL.hpp
@@ -251,34 +251,52 @@ private:
 	/**
 	 * Insert item to node, keep it balanced
 	 */
-	void insert( const T& value, Node*& nd )
+	void __insert( const T& value, Node*& nd )
 	{
         if( nd == nullptr )
             nd = new Node{ value, nullptr, nullptr };
         else if( value < nd->value )
-            insert( value, nd->left );
+            __insert( value, nd->left );
         else if( nd->value < value )
-            insert( value, nd->right );
+            __insert( value, nd->right );
         else
             ;  // Duplicate; do nothing
 	}
 
 	/**
+	 * Wrapper for insert, so that we could keep it balanced
+	 */
+	void insert( const T& value, Node*& nd )
+	{
+		__insert( value, nd );
+		balance( nd );
+	}
+
+	/**
+	 * Wrapper for remove, so that we could keep it balanced
+	 */
+	void remove( const T& value, Node*& nd )
+	{
+		__remove( value, nd );
+		balance( nd );
+	}
+
+	/**
 	 * Find and remove value from tree, keep it balanced
 	 */
-    void remove( const T & value, Node * & nd ) noexcept
+    void __remove( const T & value, Node * & nd ) noexcept
     {
         if( nd == nullptr )
             return;   // Item not found; do nothing
 
         if( value < nd->value )
-            remove( value, nd->left );
+            __remove( value, nd->left );
         else if( nd->value < value )
-            remove( value, nd->right );
+            __remove( value, nd->right );
         else if( nd->left != nullptr && nd->right != nullptr ) // Two children
         {
             nd->value = find_min( nd->right )->value;
-            remove( nd->value, nd->right );
+            __remove( nd->value, nd->right );
         }
         else
         {
@@ -420,13 +438,109 @@ private:
 	/**
 	 * Keep tree balanced, main feature of an AVL tree
 	 */
-	void balance( Node*& nd ) const
+	void balance( Node*& nd )
 	{
+		std::pair<Node*&, int> pr { find_imbalanced( nd ) };
+		int factor { pr.second };
+		if( !factor ) return; // 0 means balanced
+		Node*& imb_node { pr.first };
+
+		if( factor > 1 )	// left is larger than right
+		{
+			if( imb_node->left->right )
+			{
+				lr_rotate( imb_node );
+			}
+			else if( imb_node->left->left )
+			{
+				ll_rotate( imb_node );
+			}
+		}
+		else if( factor < -1 ) // right is larger than left
+		{
+			if( imb_node->right->left )
+			{
+				rl_rotate( imb_node );
+			}
+			else if( imb_node->right->right )
+			{
+				rr_rotate( imb_node );
+			}
+		}
+	}
+
+	/**
+	 * Find imbalanced node to work with it
+	 * Returns reference to node and it's b_factor
+	 */
+	std::pair<Node*&, int> find_imbalanced( Node*& nd ) noexcept
+	{
+		int factor { b_factor( nd ) };
+
+		if( factor < -1)
+		{
+			Node*& check { find_imbalanced( nd->right ).first };
+			return { factor ? check : nd, factor };
+		}
+		else if( factor > 1 )
+		{
+			Node*& check { find_imbalanced( nd->left  ).second };
+			return { factor ? check : nd, factor };
+		}
+		else
+		{
+			return { nd, 0 };
+		}
+	}
+
+	/**
+	 * Get balancing factor
+	 */
+	int b_factor( Node* nd ) const noexcept
+	{
+		return height( nd->left ) - height( nd->right );
+	}
+
+	/**
+	 * Left single rotation
+	 */
+	void ll_rotate( Node*& nd )
+	{
+		Node* new_right = nd;
+		nd = nd->left;
+		new_right->left = nd->right;
+		nd->right = new_right;
+	}
+
+	/**
+	 * Right single rotation
+	 */
+	void rr_rotate( Node*& nd )
+	{
+		Node* new_left = nd;
+		nd = nd->right;
+		new_left->right = nd->left;
+		nd->left = new_left;
 
 	}
 
+	/**
+	 * Right-left double rotation
+	 */
+	void rl_rotate( Node*& nd )
+	{
+		ll_rotate( nd->left );
+		rr_rotate( nd );
+	}
 
-
+	/**
+	 * Left-right double rotation
+	 */
+	void lr_rotate( Node*& nd )
+	{
+		ll_rotate( nd->right );
+		rr_rotate( nd );
+	}
 
 // ***************************************************************
 
---
+++ AVL/test_avl.cpp
@@ -0,0 +1,19 @@
+#include "AVL.hpp"
+
+#include <iostream>
+
+using namespace std;
+using namespace alda;
+
+int main()
+{
+	cout << "Insert number of nodes: ";
+	int nd_num;
+	cin >> nd_num
+
+	while(nd_num--)
+	{
+
+	}
+
+}
