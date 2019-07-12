825cfe04fe3544f301a544d28fdca6f2bb7cdd74
B1Z0N <kolausf@gmail.com>
2019-07-12 17:47:06 +0300

    United files

--- trees/red-black/redblack.hpp
+++ trees/red-black/redblack.hpp
@@ -230,6 +230,107 @@ public:
 	 * can't be performed if tree is empty
 	 */
 	class EmptyTreeError : public std::exception { };
+	
+private:
+
+// *******************SPECIFIC-TO-RED_BLACK-tree***********************
+	/**
+	 * Enumeration for coloring red-black tree
+	 */
+	enum class Color { RED, BLACK };
+
+	/**
+	 * Get balancing factor
+	 */
+	int b_factor( Node* nd ) const noexcept
+	{
+		return height( nd->left ) - height( nd->right );
+	}
+
+	/**
+	 * Right single rotation
+	 */
+	void rr_rotate( Node*& nd ) noexcept
+	{
+		Node* new_right = nd;
+		nd = nd->left;
+		new_right->left = nd->right;
+		nd->right = new_right;
+	}
+
+	/**
+	 * Left single rotation
+	 */
+	void ll_rotate( Node*& nd ) noexcept
+	{
+		Node* new_left = nd;
+		nd = nd->right;
+		new_left->right = nd->left;
+		nd->left = new_left;
+
+	}
+
+	/**
+	 * Keep tree balanced, main feature of an AVL tree
+	 */
+	void balance( Node*& imb_node ) noexcept
+	{
+		int factor { b_factor( imb_node ) };
+
+		static auto right_is_higher { [ ] ( int fact ) {
+			return fact < -1;
+		}};
+
+		static auto left_is_higher { [ ] ( int fact ) {
+			return fact > 1;
+		}};
+
+		if ( left_is_higher( factor ) )
+		{
+			int lfactor = b_factor( imb_node->left );
+
+			if ( right_is_higher( lfactor ) )
+			{
+				lr_rotate( imb_node );
+			}
+			else
+			{
+				rr_rotate( imb_node );
+			}
+		}
+		else if ( right_is_higher( factor ) )
+		{
+			int rfactor = b_factor( imb_node->right );
+
+			if ( left_is_higher( rfactor ) )
+			{
+				rl_rotate( imb_node );
+			}
+			else
+			{
+				ll_rotate( imb_node );
+			}
+		}
+	}
+
+	/**
+	 * Right-left double rotation
+	 */
+	void rl_rotate( Node*& nd ) noexcept
+	{
+		rr_rotate( nd->right );
+		ll_rotate( nd );
+	}
+
+	/**
+	 * Left-right double rotation
+	 */
+	void lr_rotate( Node*& nd ) noexcept
+	{
+		ll_rotate( nd->left );
+		rr_rotate( nd );
+	}
+// ***************************************************************
 
 private:
 
@@ -261,111 +362,188 @@ private:
 		static constexpr Node  NIL_VAL { T{ }, Color::BLACK, nullptr, nullptr, nullptr };
 		static constexpr Node* NIL     { &NIL_VAL };
 	};
-	
-// *******************SPECIFIC-TO-RED_BLACK-tree***********************
-	/**
-	 * Enumeration for coloring red-black tree
-	 */
-	enum class Color { RED, BLACK };
-
-	/**
-	 * Get balancing factor
-	 */
-	int b_factor( Node* nd ) const noexcept;
-
-	/**
-	 * Right single rotation
-	 */
-	void rr_rotate( Node*& nd ) noexcept;
-
-	/**
-	 * Left single rotation
-	 */
-	void ll_rotate( Node*& nd ) noexcept;
-
-	/**
-	 * Keep tree balanced, main feature of an AVL tree
-	 */
-	void balance( Node*& imb_node ) noexcept;
-
-	/**
-	 * Right-left double rotation
-	 */
-	void rl_rotate( Node*& nd ) noexcept;
-
-	/**
-	 * Left-right double rotation
-	 */
-	void lr_rotate( Node*& nd ) noexcept;
-
-// ***************************************************************
-
-private:
 
 	Node *root; ///< Root node
 
 	/**
 	 * Insert item to node, keep it balanced
 	 */
-	void insert( const T& value, Node*& nd );
+	void insert( const T& value, Node*& nd )
+	{
+		if ( nd == Node::NIL )
+			nd = new Node{ value, nullptr, nullptr };
+		else if ( value < nd->value )
+		{
+			insert( value, nd->left );
+			balance( nd );
+		}
+		else if ( nd->value < value )
+		{
+			insert( value, nd->right );
+			balance( nd );
+		}
+		else
+			;  // Duplicate; do nothing
+	}
 
 	/**
 	 * Find and remove value from tree, keep it balanced
 	 */
-	void remove( const T & value, Node * & nd ) noexcept;
+	void remove( const T & value, Node * & nd ) noexcept
+	{
+		if ( nd == Node::NIL )
+			return;   // Item not found; do nothing
+
+		if ( value < nd->value )
+		{
+			remove( value, nd->left );
+		}
+		else if ( nd->value < value )
+		{
+			remove( value, nd->right );
+		}
+		else if ( nd->left != Node::NIL && nd->right != Node::NIL ) // Two children
+		{
+			nd->value = find_min( nd->right )->value;
+			remove( nd->value, nd->right );
+		}
+		else
+		{
+			Node *oldnode = nd;
+			nd = ( nd->left != Node::NIL ) ? nd->left : nd->right;
+			delete oldnode;
+			return;
+		}
+
+		balance( nd );
+	}
 
 	/**
 	 * Remove all elements from tree
 	 */
-	void erase( Node*& nd ) noexcept;
+	void erase( Node*& nd ) noexcept
+	{
+		if ( nd != Node::NIL )
+		{
+			erase( nd->left );
+			erase( nd->right );
+			delete nd;
+		}
+
+		nd = Node::NIL;
+	}
 
 	/**
 	 * Return true if value is in the tree
 	 */
-	bool contains( const T & value, Node *nd ) const noexcept;
+	bool contains( const T & value, Node *nd ) const noexcept
+	{
+		while ( nd != Node::NIL )
+			if ( value < nd->value )
+				nd = nd->left;
+			else if ( nd->value < value )
+				nd = nd->right;
+			else
+				return true;    // Match
+
+		return false;   // No match
+	}
+
 	/**
 	 * Return pointer to the node with the minimal item
 	 */
-	Node* find_min( Node* nd ) const noexcept;
+	Node* find_min( Node* nd ) const noexcept
+	{
+		while ( nd->left )
+		{
+			nd = nd->left;
+		}
+
+		return nd;
+	}
 
 	/**
 	 * Return pointer to the node with the maximal item
 	 */
-	Node* find_max( Node* nd ) const noexcept;
+	Node* find_max( Node* nd ) const noexcept
+	{
+		while ( nd->right )
+		{
+			nd = nd->right;
+		}
+
+		return nd;
+	}
 
 	/**
 	 * Return height of the tree
 	 */
-	size_t height( Node* nd ) const noexcept;
+	size_t height( Node* nd ) const noexcept
+	{
+		if ( nd == Node::NIL ) return 0;
+
+		return std::max( height( nd->left  ),
+		                 height( nd->right ) ) + 1;
+	}
 
 	/**
 	 * Return number of items in the tree
 	 */
-	size_t size( Node*& nd ) const noexcept;
+	size_t size( Node*& nd ) const noexcept
+	{
+		if ( nd == Node::NIL ) return 0;
+
+		return size( nd->left ) + size( nd->right ) + 1;
+	}
 
 	/**
 	 * Call f on each item in this order: Parent-Left-Right
 	 */
 	template <typename Func>
-	void preorder( Func&& f, Node*& nd ) const;
+	void preorder( Func&& f, Node*& nd ) const
+	{
+		if ( nd == Node::NIL ) return;
+
+		f( nd->value );
+		preorder( std::forward<Func>( f ), nd->left );
+		preorder( std::forward<Func>( f ), nd->right );
+	}
 
 	/**
 	 * Call f on each item in this order: Left-Parent-Right
 	 */
 	template <typename Func>
-	void inorder( Func&& f, Node*& nd ) const;
+	void inorder( Func&& f, Node*& nd ) const
+	{
+		if ( nd == Node::NIL ) return;
+
+		inorder( std::forward<Func>( f ), nd->left );
+		f( nd->value );
+		inorder( std::forward<Func>( f ), nd->right );
+	}
 
 	/**
 	 * Call f on each item in this order: Left-Right-Parent
 	 */
 	template <typename Func>
-	void postorder( Func&& f, Node*& nd ) const;
+	void postorder( Func&& f, Node*& nd ) const
+	{
+		if ( nd == Node::NIL ) return;
+
+		postorder( std::forward<Func>( f ), nd->left );
+		postorder( std::forward<Func>( f ), nd->right );
+		f( nd->value );
+	}
 
 	/**
 	 * Return deep copy of a tree pointed by nd
 	 */
-	Node* clone( Node* nd ) const;
-
+	Node* clone( Node* nd ) const
+	{
+		return nd == Node::NIL ?
+		       Node::NIL  	   :
+		       new Node{ nd->value, clone( nd->left ), clone( nd->right ) };
+	}
 };
 
 
