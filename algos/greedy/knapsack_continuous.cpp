5f8b7d1158cce9593f8e0f4e1c58f20876d71998
B1Z0N <kolausf@gmail.com>
2019-07-28 02:31:06 +0300

    solved in O(n) time

--- algos/greedy/knapsack_continuous.cpp
+++ algos/greedy/knapsack_continuous.cpp
@@ -3,14 +3,15 @@
 #include <vector>	// vector
 #include <fstream>	// ifstream
 #include <limits>	// numeric_limits<streamsize>::max
-#include <memory>
-#include <unordered_map>
+#include <queue>
+#include <tuple>
 
+struct Material;
 
 template <typename __Istream>
-std::unordered_multimap<int, int> __input( __Istream&);
+std::pair<std::priority_queue<Material>, int> __input( __Istream&);
 
-std::unordered_multimap<int, int> input( const char* fname = nullptr );
+std::pair<std::priority_queue<Material>, int> input( const char* fname = nullptr );
 
 template <typename __Ostream>
 void __output( __Ostream&, int);
@@ -37,11 +38,11 @@ void output( int, const char* fname = nullptr );
 // Let's find the maximal sum of money that could be earned.
 // 1. First, go take maximal price per weight and steal as much
 // as you can.
-// max( 4/5, 9/3, 11/4, 2/6 ) = 11/4 = 2.75 ( $ / kg )
-// so, buddy, take 4 kg of 3rd material
-// 2. Since we have more space, we repeat 1st step.
 // max( 4/5, 9/3, 2/6 ) = 9/3 = 3 ( $ / kg )
-// take 3 kg of 2nd material
+// so, buddy, take 3 kg of 2nd material
+// 2. Since we have more space, we repeat 1st step.
+// max( 4/5, 9/3, 11/4, 2/6 ) = 11/4 = 2.75 ( $ / kg )
+// take 4 kg of 3rd material
 // 3. Again
 // max( 4/5, 2/6 ) = 4/5 = 0.8 ( $ / kg )
 // finish this and take 1 kg of 1st material, 
@@ -62,26 +63,56 @@ void output( int, const char* fname = nullptr );
 // @Output
 // 75
 //
-// ( 4 * 11 + 3 * 9 + 1 * 4 )
+// ( 3 * 9 + 4 * 11 + 1 * 4 )
 //-----------------------------------------------------------------------------
 
 
+struct Material
+{
+	int price;
+	int weight;
+
+	friend bool operator < ( const Material& a, const Material& b )
+	{ 
+		return ( a.price / a.weight ) < ( b.price / b.weight );
+	}
+};
+
+/**
+ * Solution function
+ */
+int steal( std::pair<std::priority_queue<Material>, int> mat_and_capacity )
+{
+	auto& materials = mat_and_capacity.first;
+	int capacity = mat_and_capacity.second;
+
+	int sum { };
+	while( capacity > 0 && !materials.empty() )
+	{
+		Material mt = materials.top(); materials.pop();
+		
+		sum += mt.price * 
+				( capacity < mt.weight ? capacity : mt.weight);
+		capacity -= mt.weight;
+	}
+
+	return sum;
+}
+
 int main()
 {
-	auto materials = input();
-	for( const auto& x : materials )
-		std::cout << x.first << " " << x.second;
+	output( steal( input( ) ) );
 	std::cout << '\n';
-	
+
 	return 0;
 }
 
 
 template <typename __Istream>
-std::unordered_multimap<int, int> __input( __Istream & in )
+std::pair<std::priority_queue<Material>, int> __input( __Istream & in )
 {
 	int capacity, n, price, weight;
-	std::unordered_multimap<int, int> materials;
+	std::priority_queue<Material> mtrls;
 	// in.ignore( std::numeric_limits<std::streamsize>::max, '\n' );
 
 	in >> capacity >> n;
@@ -89,14 +120,14 @@ std::unordered_multimap<int, int> __input( __Istream & in )
 	while( n-- )
 	{
 		in >> price >> weight;
-		materials.insert( std::make_pair( price, weight ) );
+		mtrls.push( { price, weight } );
 	}
 
-	return materials;
+	return { mtrls, capacity };
 }
 
 
-std::unordered_multimap<int, int> input( const char* fname )
+std::pair<std::priority_queue<Material>, int> input( const char* fname )
 {
 	if ( !fname )
 	{
@@ -105,16 +136,16 @@ std::unordered_multimap<int, int> input( const char* fname )
 	else
 	{
 		std::ifstream ifs { fname, std::ofstream::in };
-		std::unordered_multimap<int, int> text { __input( ifs ) };
+		auto mat_and_capacity { __input( ifs ) };
 		ifs.close();
 
-		return text;
+		return mat_and_capacity;
 	}
 }
 
 
 template <typename __Ostream>
-void __output( __Ostream & os, const std::string & encoded )
+void __output( __Ostream & os, int sum )
 {
 	os << sum;
 }
