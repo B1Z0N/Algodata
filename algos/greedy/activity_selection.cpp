5e0dec3de5f63c15eccd75ce3788b03cf4177c7f
B1Z0N <kolausf@gmail.com>
2019-07-29 22:27:38 +0300

    Added comments to solution algo

--- algos/greedy/activity_selection.cpp
+++ algos/greedy/activity_selection.cpp
@@ -66,7 +66,7 @@
 // 7 10 11
 //
 // @Output
-// _
+// 21
 //
 //--------------------------------------------------------------------------------------------
 
@@ -94,6 +94,7 @@ class ActivityQuery
 
 	void sort()
 	{
+		// sort by end in ascending order
 		std::sort( std::begin( quer ), std::end( quer ), 
 			[] (const Activity& fst, const Activity& snd)
 			{
@@ -162,6 +163,12 @@ int main( int argc, char* argv[] )
  */
 void ActivityQuery::__solve() const
 {
+	/**
+	 * Lambda to find the index of right-most 
+	 * non-overlaping activity
+	 * that is left to the query[ index ]
+	 * using binary search in O( lg(index) )
+	 */
 	static auto left_activity_bsearch {
 		[this] ( std::size_t index ) -> int
 		{
@@ -171,41 +178,55 @@ void ActivityQuery::__solve() const
 				std::size_t mid = ( lo + hi ) / 2;
 
 				if( quer[ mid ].end <= quer[ index ].start )
+				// if it is non-overlaping
 				{
 					if( quer[ mid + 1 ].end <= quer[ index ].start )
+					// if there are other non-overlaping activity
+					// a bit to the right, then continue searching
 						lo = mid + 1;
 					else
+					// if it is right-most non-overlaping
 						return mid;
 				}
 				else
+				// if there are overlaps, in the middle
+				// look to the left half
 					hi = mid - 1;
 			}
 
 			return -1;
+			// if quer[ index ] has no non-overlaping activites
+			// to the left of it
 		}
 	};
 
 	std::vector<std::size_t> sol ( quer.size() );
+	// sol[ i ] stores solution to first i + 1 activities
 
 	sol[0] = quer[0].weight;
+	// first solution is just it's single weight
 	for( std::size_t i = 1; i < quer.size(); i++ )
+	// find all solutions
 	{
 		std::size_t weight_with_current = quer[i].weight;
 		int j = left_activity_bsearch( i );
+		// j - index of the problem needed to solve if
+		// we want to include our i index in solution
 
 		if( j != -1 ) weight_with_current += sol[j];
+		// if there are consistent j on the left, 
+		// add max_weight of it's solution
 
-		if( sol[ i - 1 ] > weight_with_current )
-		{
-			sol[ i ] = sol[ i - 1 ];
-		}
-		else
-		{
-			sol[ i ] = weight_with_current;
-		}
+		sol[ i ] = std::max( 
+		// decide whether to include this i index into solution
+		// depending on it's weights
+			sol[ i - 1 ], 	 // weight without current
+			weight_with_current 
+		);
 	}
 
-	max_weight = sol.back();
+	max_weight = sol.back();	
+	// last index contains solution to whole problem
 }
 
 std::istream& operator>>( std::istream& is, ActivityQuery& aq )
