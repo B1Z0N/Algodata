cc0892fd5aea1c3b8d3df91216698d5768b183ac
B1Z0N <kolausf@gmail.com>
2020-01-31 02:09:26 +0200

    Completed stack algo

---
+++ hackerrank/practice/data_structures/advanced/find_maximum_index_product.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+struct Index {
+    int val;
+    int index;
+}
+
+// Complete the solve function below.
+int solve(vector<int> arr) {
+    stack<Index> st {};
+    st.push(Index{arr[0], 0});
+    int res = 0;
+    for (int i = 1; i < arr.size() && !st.empty(); ++i) {
+        int val = st.top().val;
+        while (arr[i] > val) {
+            st.pop();
+            int left = st.top().index, right = i;
+            res = max(res, left * right);
+        }
+        st.push(Index{arr[i], i});
+    }
+
+    return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int arr_count;
+    cin >> arr_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(arr_count);
+
+    for (int arr_itr = 0; arr_itr < arr_count; arr_itr++) {
+        int arr_item = stoi(arr_temp[arr_itr]);
+
+        arr[arr_itr] = arr_item;
+    }
+
+    int result = solve(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
