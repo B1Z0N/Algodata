5de4e217b5b444ac9ceff457bb94ce18737fbc2a
B1Z0N <kolausf@gmail.com>
2020-02-01 02:06:06 +0200

    Completed main algo

---
+++ competitive_p/range_queries/binary_indexed_tree.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+inline int highest_power_2(int n)
+{
+    return (n & (~(n - 1)));
+}
+
+std::vector<int> get_tree(const std::vector<int> &arr)
+{
+    std::vector<int> tree{};
+    for (int k = 1; k <= arr.size(); ++k)
+    {
+        int p = highest_power_2(k);
+        std::cout << "p: " << p << " k: " << k << '\n';
+        int sum = 0;
+        for (int i = k - p + 1; i <= k; ++i)
+        {
+            sum += arr[i - 1];
+        }
+        tree.push_back(sum);
+    }
+    return tree;
+}
+
+int sum(const std::vector<int>& tree, int k)
+{
+    int sum = 0;
+    while (k >= 1) {
+        sum += tree[k - 1];
+        k -= highest_power_2(k);
+    }
+    return sum;
+}
+
+int main()
+{
+    auto tree = get_tree(std::vector<int>{1, 3, 4, 8, 6, 1, 4, 2});
+    for (auto x : tree)
+    {
+        std::cout << x << ' ';
+    }
+    std::cout << '\n';
+
+    int k;
+    std::cin >> k;
+    std::cout << sum(tree, k) << '\n';
+    return 0;
+}
