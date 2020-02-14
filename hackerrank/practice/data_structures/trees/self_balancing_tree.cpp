ea2c4dceac0b349a0160f7c49104fea1b93a4f9f
B1Z0N <kolausf@gmail.com>
2020-02-14 16:35:56 +0200

    Added rotations and balancing

---
+++ hackerrank/practice/data_structures/trees/self_balancing_tree.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+/* Node is defined as  :
+typedef struct node {
+	int val;
+	struct node *left;
+	struct node *right;
+	int ht;
+} node;
+*/
+
+node *insert(node *root, int val) {
+
+}
