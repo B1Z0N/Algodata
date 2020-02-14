229163c8503342dfbb277f1b55fe911b8560d5ed
B1Z0N <kolausf@gmail.com>
2020-02-14 23:02:37 +0200

    Completed insertion

--- hackerrank/practice/data_structures/trees/self_balancing_tree.cpp
+++ hackerrank/practice/data_structures/trees/self_balancing_tree.cpp
@@ -10,5 +10,52 @@ typedef struct node {
 */
 
 node *insert(node *root, int val) {
+	if (root == nullptr) return new node{val};
 
+	if (root->val > val) {
+		root->left = insert(root->left, val);
+	} else if (root->val < val) {
+		root->right = insert(root->right, val);
+	} else {}
+	
+	balance(root, val);
+	return root;
 }
+
+
+right_rotate(node *&root) {
+	node* new_root = root->left;
+	root->left = root->left->right;
+        new_root->right = root;
+	root = new_root;	
+}
+
+left_rotate(node *&root) {
+	node* new_root = root->right;
+	root->right = root->right->left;
+        new_root->left = root;
+	root = new_root;	
+}
+
+bool balanced(node *root) {
+	return abs(root->left->ht - root->right->ht) < 2;
+}
+
+void balance(node *&gparent, int child_val) {
+	if (balanced(gparent)) return;
+
+	if (gparent->left->right->val == child_val) {
+		child_val = gparent->left->val;
+		left_rotate(gparent->left);
+	} else if (gparent->right->left->val == child_val) {
+		child_val = gparent->right->val;
+		right_rotate(gparent->right);
+	}
+
+	if (gparent->left->left->val == child_val) {
+		right_rotate(gparent);	
+	} else if (gparent->right->right->val == child_val) {
+		left_rotate(gparent);
+	}
+}
+
