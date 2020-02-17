b46004c8305cdc83652a332324956f14a1013dc9
B1Z0N <kolausf@gmail.com>
2020-02-17 15:26:10 +0200

    Added statement

---
+++ hackerrank/practice/data_structures/trie/contacts.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the contacts function below.
+ */
+vector<int> contacts(vector<vector<string>> queries) {
+    /*
+     * Write your code here.
+     */
+
+    
+    return {};
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int queries_rows;
+    cin >> queries_rows;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<string>> queries(queries_rows);
+    for (int queries_row_itr = 0; queries_row_itr < queries_rows; queries_row_itr++) {
+        queries[queries_row_itr].resize(2);
+
+        for (int queries_column_itr = 0; queries_column_itr < 2; queries_column_itr++) {
+            cin >> queries[queries_row_itr][queries_column_itr];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    vector<int> result = contacts(queries);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
