1999c310ae4d42b99e571e743cd648a91797582a
B1Z0N <kolausf@gmail.com>
2020-03-01 22:17:58 +0200

    Completed floyd hare-tortoise algo

--- leetcode/bsearch/find_the_duplicate_number.cpp
+++ leetcode/bsearch/find_the_duplicate_number.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int findDuplicate(vector<int>& nums) {
         std::unordered_multiset counter {nums.begin(), nums.end()};
@@ -6,5 +6,24 @@ public:
             if (counter.count(x) > 1) return x;
         
         return -1;
+    }
+};
+
+class Solution2 {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int hare = 0, tortoise = 0;
+        do {
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        } while (nums[hare] != nums[tortoise]);
+
+        hare = 0;
+        while (nums[hare] != nums[tortoise]) {
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+
+        return nums[hare];
     }
 };
