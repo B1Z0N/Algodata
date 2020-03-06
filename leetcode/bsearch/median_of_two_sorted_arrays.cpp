443f5c93bc940cfb7d2cd66e0984be0ad477a44b
B1Z0N <kolausf@gmail.com>
2020-03-07 00:19:38 +0200

    Completed generic solution working in O(lg(range)*lg(n1..nN))

--- leetcode/bsearch/median_of_two_sorted_arrays.cpp
+++ leetcode/bsearch/median_of_two_sorted_arrays.cpp
@@ -1,4 +1,4 @@
-
+// Solution for two arrays(like in problem statement)
 class Solution {
     inline bool between(const int a1, const int b, const int a2) { return a1 <= b && b <= a2; }
     
@@ -32,3 +32,47 @@ private:
         return max(a, b);
     }
 };
+
+class GenericSolution {
+public:
+    double findMedianSortedArrays(const vector<vector<int>>& arrs) {
+	    auto size = accumulate(arrs.begin(), arrs.end(), 0, [](int prev, const vector<int>& arr) { return prev + arr.size(); });
+	    if (size % 2 == 1) {
+            return findKthInSortedArrays(arrs, size / 2 + 1);
+        } else {
+            return (findKthInSortedArrays(arrs, size / 2) +
+					findKthInSortedArrays(arrs, size / 2 + 1)) / 2.0;
+        }
+    }
+
+	int findKthInSortedArrays(const vector<vector<int>>& arrs, int k) {
+		int left = numeric_limits<int>::max();
+		int right = numeric_limits<int>::min();
+		for (const auto& arr : arrs) {
+			if (!arr.empty() && arr.front() < left) left = arr.front();
+			if (!arr.empty() && arr.back() > right) right = arr.back();
+		}
+
+		while (left <= right) {
+			const int mid = left + (right - left) / 2;
+			if (match(arrs, mid, k)) right = mid - 1;
+			else left = mid + 1;
+		}
+
+		return left;
+    }
+
+	bool match(const vector<vector<int>>& arrs, int num, int target) {
+		int res = 0;
+		for (const auto& arr : arrs) {
+			if (!arr.empty())
+				res += distance(
+					arr.cbegin(),
+				   	upper_bound(arr.cbegin(), arr.cend(), num)
+				);
+        }
+
+		return res >= target;
+	}
+};
+
