9a5792808ce1b89b3ced2618f2c50652e451fb86
B1Z0N <kolausf@gmail.com>
2020-03-07 16:59:05 +0200

    Completed get_max query

--- hackerrank/practice/data_structures/trees/array_pairs.cpp
+++ hackerrank/practice/data_structures/trees/array_pairs.cpp
@@ -19,17 +19,30 @@ protected:
 	static inline int right(int i) { return 2 * i + 1; }
 
 	vector<int> tree;
-
+	size_t n;
 public:
-	MaxRangeTree(const vector<int>& arr) :tree{max_tree(arr)} {}
+	MaxRangeTree(const vector<int>& arr) :tree{max_tree(arr)}, n{arr.size()} {}
 
+	// range starting from zero
 	int get_max(int a, int b) {
-		return 0;
+		a += n; b += n;
+		int res = INT_MIN;
+		while (a <= b) {
+			if (a % 2 == 1 /* right */) res = max(res, tree[a++ - 1]);
+			if (b % 2 == 0 /* left  */) res = max(res, tree[b-- - 1]);
+			
+			a = parent(a); b = parent(b);
+		}
+
+		return res;
 	}
 };
 
 int main() {
 	MaxRangeTree mrt { {5, 8, 6, 3, 2, 7, 2, 6} };
+	int a, b;
+	cin >> a >> b;
+	cout << "Maximal in range [" << a << ", " << b << "] " << mrt.get_max(a, b) << '\n';
 
 	return 0;
 }
